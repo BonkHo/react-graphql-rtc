e45181070f13b9aeeae62bc450d75c15e8e20f65
John <john21225@gmail.com>
2021-11-25 21:00:19 -0800

    PubSub subscription for async communication

--- server/server.js
+++ server/server.js
@@ -1,4 +1,4 @@
-const { GraphQLServer } = require("graphql-yoga");
+const { GraphQLServer, PubSub } = require("graphql-yoga");
 
 const messages = [];
 
@@ -16,7 +16,16 @@ const typeDefs = `
     type Mutation {
         postMessage(user: String!, content: String!): ID!
     }
+
+    type Subscription {
+        messages: [Message!]!
+    }
 `;
+
+const subscribers = [];
+const onMessagesUpdate = (fn) => {
+    subscribers.push(fn);
+};
 
 const resolvers = {
     Query: {
@@ -26,12 +35,25 @@ const resolvers = {
         postMessage: (parent, { user, content }) => {
             const id = messages.length;
             messages.push({ id, user, content });
+            subscribers.forEach((fn) => fn());
             return id;
+        },
+    },
+    Subscription: {
+        messages: {
+            subscribe: (parent, args, { pubsub }) => {
+                // Generates a random number with decimals and we slice the decimals
+                const channel = Math.random().toString(36).slice(2, 15);
+                onMessagesUpdate(() => pubsub.publish(channel, { messages }));
+                setTimeout(() => pubsub.publish(channel, { messages }), 0);
+                return pubsub.asyncIterator(channel);
+            },
         },
     },
 };
 
-const server = new GraphQLServer({ typeDefs, resolvers });
+const pubsub = new PubSub();
+const server = new GraphQLServer({ typeDefs, resolvers, context: { pubsub } });
 server.start(({ port }) => {
     console.log(`Server is running on http://localhost:${port}`);
 });
